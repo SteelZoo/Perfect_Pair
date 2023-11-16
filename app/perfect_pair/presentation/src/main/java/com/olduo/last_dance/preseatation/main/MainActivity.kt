2ac3b7d1b50c42c1be5633927b74a220769f84a2
steelzoo <cjfwn96@gmail.com>
2023-11-16 22:53:39 +0900

    rename: MainActivity 패키지 이동

--- app/perfect_pair/presentation/src/main/java/com/olduo/last_dance/preseatation/main/MainActivity.kt
+++ app/perfect_pair/presentation/src/main/java/com/olduo/last_dance/preseatation/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.olduo.last_dance.preseatation
+package com.olduo.last_dance.preseatation.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.olduo.last_dance.preseatation.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
