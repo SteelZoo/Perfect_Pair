aad9d85070e5ba609344a4f5e6289199d037f4a9
steelzoo <cjfwn96@gmail.com>
2023-11-16 17:17:48 +0900

    init project

---
+++ app/perfect_pair/app/src/main/java/com/olduo/last_dance/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.olduo.last_dance
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
