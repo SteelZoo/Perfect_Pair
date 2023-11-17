2a862bdba408330e01ac22ffee701281c23373b8
steelzoo <cjfwn96@gmail.com>
2023-11-17 11:28:27 +0900

    create: signin, signup

---
+++ app/perfect_pair/presentation/src/main/java/com/olduo/last_dance/preseatation/login/signin/SignInFragment.kt
@@ -0,0 +1,22 @@
+package com.olduo.last_dance.preseatation.login.signin
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.olduo.last_dance.preseatation.R
+
+class SignInFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sign_in, container, false)
+    }
+}
