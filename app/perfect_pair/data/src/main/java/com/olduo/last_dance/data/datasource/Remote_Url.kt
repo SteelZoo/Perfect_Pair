c12e553d7c53092e47b813d0a07c20bb3db8d748
steelzoo <cjfwn96@gmail.com>
2023-11-20 01:55:32 +0900

    feat: Nework Module 생성 및  UserService 주입

---
+++ app/perfect_pair/data/src/main/java/com/olduo/last_dance/data/datasource/Remote_Url.kt
@@ -0,0 +1,3 @@
+package com.olduo.last_dance.data.datasource
+
+const val PERFECT_PAIR_BASE_URL = "192.168.0.6:1124/rest/"
---
+++ app/perfect_pair/data/src/main/java/com/olduo/last_dance/data/datasource/remote/UserRemoteDatasource.kt
@@ -0,0 +1,9 @@
+package com.olduo.last_dance.data.datasource.remote
+
+import javax.inject.Inject
+
+class UserRemoteDatasource @Inject constructor(
+
+) {
+
+}
